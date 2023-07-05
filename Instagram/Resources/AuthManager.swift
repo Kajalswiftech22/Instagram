0c7ae181baee89863fd11f6566a71057943fff53
intern <intern@Puran-iMac.local>
2023-07-05 13:52:45 +0545

    logout

--- Instagram/Resources/AuthManager.swift
+++ Instagram/Resources/AuthManager.swift
@@ -53,6 +53,19 @@ public class AuthManager {
         }
         else if let username = username {
             print(username)
-        }
+    }
+}
+
+public func logOut(completion: (Bool) -> Void) {
+    do{
+        try Auth.auth().signOut()
+        completion(true)
+        return
+    }
+    catch{
+        print(error)
+        completion(false)
+        return
+    }
     }
 }
